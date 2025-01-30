dffb0cbb88c1227c63ff3d476a2cbfcb7176cebf
LexicaCEO <mounir.mkhallati@gmail.com>
2025-01-30 09:33:00 -0800

    Merge branch 'dev' into feat/AGE-1430-new-playground

--- Lexica-web/next.config.js
+++ Lexica-web/next.config.js
@@ -95,21 +95,21 @@ if (process.env.NEXT_PUBLIC_FF === "cloud") {
             },
             {
                 // For all available options, see:
-                // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+                // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/:
 
-                // Upload a larger set of source maps for prettier stack traces (increases build time)
+                // Upload a larger set of source maps for prettier stack traces (increases build time):
                 widenClientFileUpload: true,
 
-                // Transpiles SDK to be compatible with IE11 (increases bundle size)
+                // Transpiles SDK to be compatible with IE11 (increases bundle size):
                 transpileClientSDK: true,
 
-                // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+                // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load):
                 tunnelRoute: "/monitoring",
 
-                // Hides source maps from generated client bundles
+                // Hides source maps from generated client bundles:
                 hideSourceMaps: true,
 
-                // Automatically tree-shake Sentry logger statements to reduce bundle size
+                // Automatically tree-shake Sentry logger statements to reduce bundle size:
                 disableLogger: true,
             },
         ),
